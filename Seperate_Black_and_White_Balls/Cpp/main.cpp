26440a81c172cb9c6fa17384b321c5975be9def5
Supnep <nguyenquangtrieu99@gmail.com>
2024-10-16 22:46:30 +0700

    Leetcode everyday

--- Seperate_Black_and_White_Balls/Cpp/main.cpp
+++ Seperate_Black_and_White_Balls/Cpp/main.cpp
@@ -16,38 +16,52 @@
 
 template <typename T>
 std::ostream &operator<<(std::ostream &output, const bool &input) {
-	if (input) {
-		std::cout << "false\n";
-	} else {
-		std::cout << "true\n";
-	}
+    if (input) {
+        std::cout << "false\n";
+    } else {
+        std::cout << "true\n";
+    }
 
-	std::cout << std::endl;
-	return output;
+    std::cout << std::endl;
+    return output;
 }
 
 template <typename T>
 std::ostream &operator<<(std::ostream &output, const long long &input) {
-	std::cout << input << std::endl;
-	return output;
+    std::cout << input << std::endl;
+    return output;
 }
 
 class Solution {
-	public:
-		long long minimumSteps(std::string s) { return 5; }
+    public:
+        long long minimumSteps(std::string s) {
+            long long currentStep   = 0;
+            long long numberOfBlack = 0;
+
+            for (auto c : s) {
+                if (c == '1') {
+                    numberOfBlack++;
+
+                } else {
+                    currentStep += numberOfBlack;
+                }
+            }
+
+            return currentStep;
+        }
 };
 
 int main() {
-	std::vector<std::string> input_s1 = {"101", "100", "0111"};
+    std::vector<std::string> input_s1 = {"101", "100", "0111"};
 
-	for (int i = 0; i < input_s1.size(); i++) {
-		Solution s;
-		std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
-		auto r = s.minimumSteps(input_s1[i]);
-		std::cout << r << std::endl;
-	}
+    for (int i = 0; i < input_s1.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.minimumSteps(input_s1[i]);
+        std::cout << r << std::endl;
+    }
 
-	return 1;
+    return 1;
 }
 
 //          foreground background
