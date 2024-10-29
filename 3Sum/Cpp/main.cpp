6e9730d962b1d58ca04cd6bdefefb2365cd206e6
Supnep <nguyenquangtrieu99@gmail.com>
2024-10-30 01:32:57 +0700

    Leetcode everyday

--- 3Sum/Cpp/main.cpp
+++ 3Sum/Cpp/main.cpp
@@ -16,61 +16,69 @@
 #include <unordered_set>
 
 template <typename T>
-std::ostream &operator<<(std::ostream &output, const long long &input) {
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
     std::cout << input << std::endl;
     return output;
 }
 
 template <typename T>
-std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
     std::cout << "[";
 
-    for (auto it = input.begin(); it != input.end(); ++it) {
-        if (std::next(it) == input.end()) {
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
             std::cout << *it;
-        } else {
+        else
             std::cout << *it << ",";
-        }
-    }
 
     std::cout << "]" << std::endl;
 
     return output;
 }
 
-class Solution {
+class Solution
+{
     public:
-        std::vector<std::vector<int>> threeSum(std::vector<int> &nums) {
+        std::vector<std::vector<int>> threeSum(std::vector<int> &nums)
+        {
             std::vector<std::vector<int>> result;
-            std::unordered_map<int, int> mapDict;
+            std::sort(nums.begin(), nums.end());
+            std::set<std::vector<int>> s;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                int j            = i + 1;
+                int k            = nums.size() - 1;
+                int completement = 0 - nums[i];
 
-            for (auto n : nums) {
-                mapDict[n]++;
-            }
-
-            for (int i = 0; i < nums.size(); i++) {
-                int diff = 0 - nums[i];
-                for (int j = i; j < nums.size(); j++) {
-                    if (j == i)
-                        continue;
-
-                    if (mapDict.find(diff - nums[j]) != mapDict.end() && mapDict[diff - nums[j]] > 0 && mapDict[nums[j]] > 0 && mapDict[nums[i]] > 0) {
-                        result.push_back({nums[i], nums[j], mapDict[diff - nums[j]]});
-                        mapDict[diff - nums[j]]--;
-                        mapDict[nums[j]]--;
-                        mapDict[nums[i]]--;
+                while (j < k)
+                    if (nums[j] + nums[k] == completement)
+                    {
+                        s.insert({nums[i], nums[j], nums[k]});
+                        j++;
+                        k--;
                     }
-                }
+                    else if (nums[j] + nums[k] > completement)
+                        k--;
+                    else
+                        j++;
             }
+
+            for (auto triples : s)
+                result.push_back(triples);
 
             return result;
         }
 };
-int main() {
+
+int main()
+{
     std::vector<std::vector<int>> input = {{-1, 0, 1, 2, -1, -4}, {0, 1, 1}, {0, 0, 0}};
     std::vector<int> target = {9, 6, -1};
 
-    for (int i = 0; i < input.size(); i++) {
+    for (int i = 0; i < input.size(); i++)
+    {
         Solution s;
         std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
         auto r = s.threeSum(input[i]);
