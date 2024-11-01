1f8be70c86ecf053ff657539853bbb2bba520b69
Supnep <nguyenquangtrieu99@gmail.com>
2024-11-01 23:35:05 +0700

    Leetcode everyday

---
+++ Delete_Character_to_Make_Fancy_string/Cpp/main.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        std::string makeFancyString(std::string s)
+        {
+            if (s.size() < 3)
+                return s;
+
+            std::stringstream ss;
+            ss << s[0] << s[1];
+
+            for (int i = 2; i < s.size(); i++)
+            {
+                if (s[i] == s[i - 1] && s[i] == s[i - 2])
+                {
+                    continue;
+                }
+                ss << s[i];
+            }
+
+            return ss.str();
+        }
+};
+
+int main()
+{
+    std::vector<std::string> input = {"leeetcode", "aaabaaaa", "aab"};
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.makeFancyString(input[i]);
+        std::cout << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
