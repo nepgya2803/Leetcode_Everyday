e189195a2e1e49b7975cd501f0870c406966b9cf
trieu.nguyen <trieu.nguyen@lge.com>
2024-11-05 16:40:36 +0700

    Leetcode

---
+++ Minimum_Number_of_Change_to_Make_Binary_String_Beatiful/Cpp/main.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        int minChanges(std::string s)
+        {
+            int count = 0;
+            int i     = 1;
+            while (i < s.size())
+            {
+                if (s[i] == '0' && s[i - 1] == '1' || s[i] == '1' && s[i - 1] == '0')
+                    count++;
+                i += 2;
+            }
+
+            return count;
+        }
+};
+
+int main()
+{
+    std::vector<std::string> input = {
+        "1001",
+        "10",
+        "0000",
+    };
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.minChanges(input[i]);
+        std::cout << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
