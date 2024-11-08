b7dda69a70e4ce1d8f5645cf3883ec2b8350e7ad
Supnep <nguyenquangtrieu99@gmail.com>
2024-11-08 22:52:22 +0700

    Leetcode everyday

---
+++ Maximum_XOR_for_Each_Query/Cpp/main.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+#include <bits/stdc++.h>
+#include <cstdlib>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        std::vector<int> getMaximumXor(std::vector<int> &nums, int maximumBit)
+        {
+            std::vector<int> res;
+
+            int bit = std::pow(2, maximumBit) - 1;
+
+            for (int i = 1; i < nums.size(); i++)
+            {
+                nums[i] ^= nums[i - 1];
+            }
+
+            for (int i = nums.size() - 1; i >= 0; i--)
+            {
+                res.push_back(nums[i] ^ bit);
+            }
+
+            return res;
+        }
+};
+
+int main()
+{
+    std::vector<std::vector<int>> input = {
+        {0, 1, 1, 3},
+        {2, 3, 4, 7},
+        {0, 1, 2, 2, 5, 7},
+    };
+
+    std::vector<int> k = {2, 3, 3};
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.getMaximumXor(input[i], k[i]);
+        std::cout << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
