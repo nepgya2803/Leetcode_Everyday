fbc06f5301585c9b9ba377f59fb6adcab54e0117
trieu.nguyen <trieu.nguyen@lge.com>
2024-11-20 16:27:48 +0700

    Leetcode

---
+++ Take_K_of_Each_Character_From_Left_and_Right/Cpp/main.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        int takeCharacters(std::string s, int k)
+        {
+            int ans = 0;
+            for (int i = 0; i < s.size(); i++)
+
+                return ans;
+        }
+};
+
+int main()
+{
+    std::vector<std::string> input = {
+        "aabaaaacaabc",
+        "a",
+    };
+
+    std::vector<int> k = {
+        2,
+        1,
+    };
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.takeCharacters(input[i], k[i]);
+        std::cout << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
