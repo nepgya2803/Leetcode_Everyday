487d3c2ec9ccd9c7c614fb6d8373ba9ff347559c
Supnep <nguyenquangtrieu99@gmail.com>
2024-11-25 22:50:50 +0700

    Leetcode everyday

--- Sliding_Puzzle/Cpp/main.cpp
+++ Sliding_Puzzle/Cpp/main.cpp
@@ -38,16 +38,57 @@ std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
     return output;
 }
 
+static const std::vector<std::vector<int>> direction = {
+    {1, 3},
+    {0, 2, 4},
+    {1, 5},
+    {0, 4},
+    {1, 3, 5},
+    {2, 4}
+};
+static bool visited[543211];
+static const int p10[7] = {1, 10, 100, 1000, 10000, 100000, 1000000};
 class Solution
 {
     public:
-        int slidingPuzzle(std::vector<std::vector<int>> &board)
+        int slidingPuzzle(std::vector<std::vector<int>> const &board)
         {
-            int move = 0;
-
-            
-
-            return move == 0 ? -1 : move;
+            memset(visited, false, sizeof(visited));
+            std::queue<int> q; // [idx|xyxyxy]
+            int idx = 0, value = 0;
+            for (int i = 0; i < 6; ++i)
+            {
+                if (board[i / 3][i % 3] == 0)
+                    idx = i;
+                value += board[i / 3][i % 3] * p10[i];
+            }
+            visited[value] = true;
+            q.emplace(value + idx * p10[6]);
+            int moves = 0;
+            while (!q.empty())
+            {
+                int n = q.size();
+                while (n--)
+                {
+                    int current = q.front();
+                    int idx     = current / p10[6];
+                    current %= p10[6];
+                    q.pop();
+                    if (current == 54321)
+                        return moves;
+                    for (auto nidx : direction[idx])
+                    {
+                        int v    = (current / p10[nidx]) % 10;
+                        int next = current + v * (p10[idx] - p10[nidx]);
+                        if (visited[next])
+                            continue;
+                        visited[next] = true;
+                        q.emplace(next + nidx * p10[6]);
+                    }
+                }
+                ++moves;
+            }
+            return -1;
         }
 };
 
