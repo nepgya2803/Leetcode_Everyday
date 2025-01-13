760d89d01249587c722fa6fbfaa4b92c69367c1f
supnep <nguyenquangtrieu99@gmail.com>
2025-01-13 10:01:29 +0700

    x

---
+++ Minimum_Length_of_String_After_Operations/Cpp/main.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit
+{
+    public:
+        std::string s;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        int minimumLength(std::string s)
+        {
+            std::unordered_map<char, int> count;
+
+            for (const char &c : s)
+            {
+                count[c]++;
+            }
+
+            int deleteCount = 0;
+            for (const std::pair<char, int> &freq : count)
+            {
+                if (freq.second % 2 == 1)
+                {
+                    deleteCount += freq.second - 1;
+                }
+
+                if (freq.second % 2 == 0)
+                {
+                    deleteCount += freq.second - 2;
+                }
+            }
+
+            return s.size() - deleteCount;
+        }
+};
+
+int main()
+{
+    std::vector<TestSuit> testcase {
+        {"abaacbcbb"},
+        {"aa"},
+    };
+
+    for (int i = 0; i < testcase.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.minimumLength(testcase[i].s);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
