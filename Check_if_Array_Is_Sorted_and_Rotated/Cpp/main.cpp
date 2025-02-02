e8027c76e15a6bcc738298979c6568efc6abcba9
Supnep <nguyenquangtrieu99@gmail.com>
2025-02-02 19:52:48 +0700

    x

---
+++ Check_if_Array_Is_Sorted_and_Rotated/Cpp/main.cpp
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <unordered_set>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit
+{
+    public:
+        std::vector<int> nums;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        bool check(std::vector<int> &nums)
+        {
+            int size                     = nums.size();
+            std::vector<int> copiedArray = nums;
+            std::sort(copiedArray.begin(), copiedArray.end());
+
+            for (int rotationOffSet = 0; rotationOffSet < size; rotationOffSet++)
+            {
+                bool isMatch = true;
+                for (int i = 0; i < size; i++)
+                {
+                    if (nums[(rotationOffSet + i) % size] != copiedArray[i])
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+
+                if (isMatch)
+                    return true;
+            }
+
+            return false;
+        }
+};
+
+int main()
+{
+    std::vector<TestSuit> testcase {
+        {{3, 4, 5, 1, 2}},
+        {{2, 1, 3, 4}},
+        {{1, 2, 3}},
+    };
+
+    for (int i = 0; i < testcase.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.check(testcase[i].nums);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
