4fd535bd4cca6aa12d33bcf8615772ac141eb125
Supnep <nguyenquangtrieu99@gmail.com>
2025-02-10 20:49:35 +0700

    x

---
+++ Clear_Digits/Cpp/main.cpp
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit
+{
+    public:
+        std::string s;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input)
+{
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
+{
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution
+{
+    public:
+        std::string clearDigits(std::string s)
+        {
+            std::basic_string<char> result;
+            int length = s.size();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (isdigit(s[i]))
+                {
+                    if (result.size() > 0)
+                    {
+                        result.erase(result.size() - 1);
+                    }
+                }
+                else
+                {
+                    result.insert(result.end(), 1, s[i]);
+                }
+            }
+
+            return result;
+        }
+};
+
+int main()
+{
+    std::vector<TestSuit> testcase {
+        {"abc"},
+        {"cb34"},
+        {"r4tt67t"},
+    };
+
+    for (int i = 0; i < testcase.size(); i++)
+    {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.clearDigits(testcase[i].s);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
