34c900956ac3e2de7838b228bd88878b9c397b88
supnep <nguyenquangtrieu99@gmail.com>
2025-02-21 11:58:40 +0700

    x

--- Sum_of_Two_Values/Cpp/main.cpp
+++ Sum_of_Two_Values/Cpp/main.cpp
@@ -15,12 +15,34 @@
 #include <unordered_set>
 #include <vector>
 #include <string_view>
+#include <fstream>
 
-void Reading(std::string path)
-{
-    
+void Solution(int &target, std::vector<int> &arr) {
+    std::unordered_map<int, int> collector;
+
+    for (int i = 0; i < arr.size(); i++) {
+        int remainder = target - arr[i];
+
+        if (collector.find(remainder) != collector.end()) {
+            std::cout << collector[remainder] << " " << i + 1 << std::endl;
+            return;
+        }
+
+        collector[arr[i]] = i + 1;
+    }
+
+    std::cout << "IMPOSSIBLE" << std::endl;
 }
 
 int main() {
+    int array_size, target = 0;
+    std::cin >> array_size >> target;
+    std::vector<int> arr(array_size);
+    for (int i = 0; i < array_size; i++) {
+        std::cin >> arr[i];
+    }
 
+    Solution(target, arr);
+
+    return 1;
 }
