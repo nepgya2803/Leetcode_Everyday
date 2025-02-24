5143cc401429379872d1c139ea94b2d8bcfc4bb3
Supnep <nguyenquangtrieu99@gmail.com>
2025-02-25 02:09:51 +0700

    x

--- Remove_the_Ends/Cpp/main.cpp
+++ Remove_the_Ends/Cpp/main.cpp
@@ -46,9 +46,48 @@ std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
     return output;
 }
 
-void Solve(int &n, std::vector<ll> &nums)
+/*
+Solution
+*/
+void Solve(int &size, std::vector<ll> &nums)
 {
-    std::cout << nums << std::endl;
+    std::vector<ll> prefix(size), suffix(size);
+
+    if (nums[0] > 0)
+    {
+        prefix[0] = nums[0];
+    }
+
+    for (int i = 1; i < size; i++)
+    {
+        prefix[i] = prefix[i - 1];
+        if (nums[i] > 0)
+        {
+            prefix[i] += nums[i];
+        }
+    }
+
+    if (nums[size - 1] < 0)
+    {
+        suffix[size - 1] = -nums[size - 1];
+    }
+
+    for (int i = size - 2; i >= 0; i--)
+    {
+        suffix[i] = suffix[i + 1];
+        if (nums[i] < 0)
+        {
+            suffix[i] -= nums[i];
+        }
+    }
+
+    ll result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        result = std::max(result, prefix[i] + suffix[i]);
+    }
+
+    std::cout << result << std::endl;
 }
 
 int main()
