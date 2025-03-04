65cc34bc6bd670d467352711f7cf908c4492df50
Supnep <nguyenquangtrieu99@gmail.com>
2025-03-05 02:29:52 +0700

    x

--- Biridian_Forest/Cpp/main.cpp
+++ Biridian_Forest/Cpp/main.cpp
@@ -6,10 +6,14 @@
     std::cin.tie(nullptr);
 #define pii std::pair<int, int>
 #define mp std::make_pair
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
 // ----------------- MACRO ------------------//
 
 // ----------------- TYPEDEF ------------------//
-typedef std::vector<std::vector<char>> Matrix;
+typedef std::vector<int> vi;
+typedef std::vector<std::string> vs;
+typedef std::vector<std::vector<char>> vvc;
+typedef std::vector<std::vector<int>> vvi;
 typedef std::vector<char> OneLine;
 // ----------------- CLOSE ------------------//
 
@@ -42,33 +46,38 @@ Solution
 
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
+vs map(1005);
+vvi distance(1005, vi(1005));
 
-int Solve(int &x, int &y, Matrix &map, pii &protagonist, pii &door) {
+int Solve(int &x, int &y, pii &now, pii &goal) {
     std::queue<pii> q;
-    q.push(door);
+    q.push(now);
 
     while (!q.empty()) {
-        door = q.front();
+        now = q.front();
         q.pop();
         for (int i = 0; i < 4; i++) {
-            pii next = door;
+            pii next = now;
             next.first += dx[i];
             next.second += dy[i];
-            if (next.first < 0 || next.first >= x || next.second < 0 || next.second >= y ||
-                map[next.first][next.second] == 'T')
+            if (next.first < 0 || next.first >= x)
                 continue;
-            if (map[next.first][next.second] > map[door.first][door.second] + 1) {
-                map[next.first][next.second] > map[door.first][door.second] + 1;
+            if (next.second < 0 || next.second >= y)
+                continue;
+            if (map[next.first][next.second] == 'T')
+                continue;
+            if (distance[next.first][next.second] > distance[now.first][now.second] + 1) {
+                distance[next.first][next.second] = distance[now.first][now.second] + 1;
                 q.push(next);
             }
         }
     }
 
     int battle = 0;
-    int pp = map[protagonist.first][protagonist.second];
+    int pp = distance[goal.first][goal.second];
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < y; j++) {
-            if (map[i][j] <= '9' && map[i][j] >= '0' && map[i][j] - 1 < pp) {
+            if (map[i][j] <= '9' && map[i][j] >= '0' && distance[i][j] - 1 < pp) {
                 battle += map[i][j] - '0';
             }
         }
@@ -81,22 +90,23 @@ int main() {
     fast;
     int x, y = 0;
     std::cin >> x >> y;
-    Matrix m(x, OneLine(y));
-    pii pos, door;
-    std::string row = "";
-
+    pii now, goal;
+    std::cout << x << y;
     for (int i = 0; i < x; i++) {
-        std::cin >> row;
-        for (int s = 0; s < y; s++) {
-            m[i][s] = row[s];
-            if (row[s] == 'E')
-                door = mp(i, s);
-            if (row[s] == 'S')
-                pos = mp(i, s);
+        std::cin >> map[i];
+
+        for (int j = 0; j < y; j++) {
+            if (map[i][j] == 'E') {
+                now = mp(i, j);
+                distance[i][j] = 0;
+            }
+
+            if (map[i][j] == 'S')
+                goal = mp(i, j);
         }
     }
 
-    int res = Solve(x, y, m, pos, door);
+    int res = Solve(x, y, now, goal);
 
     std::cout << res << std::endl;
 
