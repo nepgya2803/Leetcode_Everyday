a9b29d09baff7871a4c46e4a0a2380f32ed11acf
Supnep <nguyenquangtrieu99@gmail.com>
2025-03-05 23:54:33 +0700

    x

--- Find_Minimum_in_Rotated_Sorted_Array/Cpp/main.cpp
+++ Find_Minimum_in_Rotated_Sorted_Array/Cpp/main.cpp
@@ -12,24 +12,33 @@
 #include <sstream>
 #include <stack>
 #include <unordered_map>
-#include <vector>
 #include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> nums;
+};
 
 template <typename T>
-std::ostream &operator<<(std::ostream &output, const long long &input)
-{
+std::ostream &operator<<(std::ostream &output, const long long &input) {
     std::cout << input << std::endl;
     return output;
 }
 
 template <typename T>
-std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
-{
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
     std::cout << "[";
 
     for (auto it = input.begin(); it != input.end(); ++it)
         if (std::next(it) == input.end())
-            std::cout << *it;
+            std::cout << std::boolalpha << *it;
         else
             std::cout << *it << ",";
 
@@ -38,30 +47,33 @@ std::ostream &operator<<(std::ostream &output, const std::vector<T> &input)
     return output;
 }
 
-class Solution
-{
-    public:
-        int findMin(std::vector<int> &nums)
-        {
-            int mid = nums.size() / 2;
-            
+class Solution {
+public:
+    int findMin(std::vector<int> &nums) {
+        int size = nums.size();
+
+        int min = INT16_MAX;
+
+        for (int i = 0; i < nums.size(); i++) {
+            min = std::min(min, nums[i]);
         }
+
+        return min;
+    }
 };
 
-int main()
-{
-    std::vector<std::vector<int>> input = {
-        {3, 4, 5, 1, 2},
-        {4, 5, 6, 7, 0, 1, 2},
-        {11, 13, 15, 17},
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 3, 4, 5, 1, 2 } },
+        { { 4, 5, 6, 7, 0, 1, 2 } },
+        { { 11, 13, 15, 17 } },
     };
 
-    for (int i = 0; i < input.size(); i++)
-    {
+    for (int i = 0; i < testcase.size(); i++) {
         Solution s;
         std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
-        auto r = s.findMin(input[i]);
-        std::cout << r << std::endl;
+        auto r = s.findMin(testcase[i].nums);
+        std::cout << std::boolalpha << r << std::endl;
     }
 
     return 1;
