0cec84b796ee388ed0191eb047bb576acfdfebe4
Supnep <nguyenquangtrieu99@gmail.com>
2025-03-08 11:05:19 +0700

    x

---
+++ Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks/Cpp/main.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::string blocks;
+    int k;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    int minimumRecolors(std::string blocks, int k) {
+        int n = blocks.size();
+        int min = INT16_MAX;
+
+        for (int i = 0; i <= n - k; i++) {
+            std::string::difference_type f = std::count(blocks.begin() + i, blocks.begin() + i + k, 'W');
+            int occurence = static_cast<int>(f);
+            min = std::min(min, occurence);
+        }
+
+        return min;
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { "WBBWWBBWBW", 7 },
+        { "WBWBBBW", 2 },
+        { "BWWWBB", 6 },
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.minimumRecolors(testcase[i].blocks, testcase[i].k);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
