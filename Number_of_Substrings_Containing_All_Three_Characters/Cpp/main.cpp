f0066e5bab0278242c3a0dbe3767d28f093af9ac
Supnep <nguyenquangtrieu99@gmail.com>
2025-03-12 00:37:54 +0700

    x

---
+++ Number_of_Substrings_Containing_All_Three_Characters/Cpp/main.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::string s;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    int numberOfSubstrings(std::string s) {
+        int sub = 0;
+        int length = s.size();
+        std::vector<int> last_pos(3, -1);
+
+        for (int pos = 0; pos < length; pos++) {
+            last_pos[s[pos] - 'a'] = pos;
+
+            sub += 1 + std::min({ last_pos[0], last_pos[1], last_pos[2] });
+        }
+
+        return sub;
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { "abcabc" },
+        { "aaacb" },
+        { "abc" },
+
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.numberOfSubstrings(testcase[i].s);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
