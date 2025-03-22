c31dc385bc0cd4558537157499d2d923a204594d
Supnep <nguyenquangtrieu99@gmail.com>
2025-03-22 16:19:04 +0700

    x

---
+++ Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I/Cpp/main.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+#include <functional>
+#include <numeric>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> nums;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    int minOperations(std::vector<int> &nums) {
+        int size = nums.size();
+        int count = 0;
+
+        for (int i = 2; i < size; i++) {
+            if (nums[i - 2] == 0) {
+                count++;
+                nums[i - 2] ^= 1;
+                nums[i - 1] ^= 1;
+                nums[i] ^= 1;
+            }
+        }
+
+        int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        if (sum != size) {
+            return -1;
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 0, 1, 1, 1, 0, 0 } },
+        { { 0, 1, 1, 1 } },
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.minOperations(testcase[i].nums);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
