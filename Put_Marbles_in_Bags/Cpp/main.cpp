20c95b44dfca2d91e4abe23878c8b21117ecdcb2
Supnep <nguyenquangtrieu99@gmail.com>
2025-03-31 22:55:06 +0700

    x

---
+++ Put_Marbles_in_Bags/Cpp/main.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+#include <functional>
+#include <numeric>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> weights;
+    int k;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    long long putMarbles(std::vector<int> &weights, int k) {
+        int n = weights.size();
+        std::vector<int> pairWeight(n - 1, 0);
+
+        for (int index = 0; index < n - 1; index++) {
+            pairWeight[index] = weights[index] + weights[index + 1];
+        }
+
+        std::sort(pairWeight.begin(), pairWeight.end());
+        int answer = 0;
+
+        for (int i = 0; i < k - 1; i++) {
+            answer += pairWeight[n - 2 - i] - pairWeight[i];
+        }
+
+        return answer;
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 1, 3, 5, 1 }, 2 },
+        { { 1, 3 }, 2 },
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.putMarbles(testcase[i].weights, testcase[i].k);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
