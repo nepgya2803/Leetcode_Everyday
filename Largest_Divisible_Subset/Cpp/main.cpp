bd9a24c5eb4fcd27a51202d7fcd7c6fe43db9ebe
Supnep <nguyenquangtrieu99@gmail.com>
2025-04-07 12:51:15 +0700

    x

---
+++ Largest_Divisible_Subset/Cpp/main.cpp
@@ -0,0 +1,105 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+#include <functional>
+#include <numeric>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> nums;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    std::vector<int> largestDivisibleSubset(std::vector<int> &nums) {
+        int n = nums.size();
+        std::sort(nums.begin(), nums.end());
+        std::vector<int> dp(n, 1), prev(n, -1);
+        int maxi = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+
+            if (dp[i] > dp[maxi])
+                maxi = i;
+        }
+
+        std::vector<int> res;
+        for (int i = maxi; i >= 0; i = prev[i]) {
+            res.push_back(nums[i]);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 1, 2, 3 } },
+        { { 1, 2, 4, 8 } },
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.largestDivisibleSubset(testcase[i].nums);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
---
+++ Partition_Equal_Subset_Sum/Cpp/main.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+#include <functional>
+#include <numeric>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> nums;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    bool canPartition(std::vector<int> &nums) {
+        int sum = 0;
+
+        for (auto num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0)
+            return false;
+
+        int targetSum = sum / 2;
+
+        std::vector<bool> dp(targetSum + 1, false);
+        dp[0] = true;
+        for (auto num : nums) {
+            for (int currSum = targetSum; currSum >= num; currSum--) {
+                dp[currSum] = dp[currSum] || dp[currSum - num];
+                if (dp[targetSum])
+                    return true;
+            }
+        }
+
+        return dp[targetSum];
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 1, 5, 11, 5 } },
+        { { 1, 2, 3, 5 } },
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.canPartition(testcase[i].nums);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
