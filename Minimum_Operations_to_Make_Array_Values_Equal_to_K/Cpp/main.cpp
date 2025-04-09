1972b985a3cb92929d0ef26f213c509d566ef0a3
supnep <nguyenquangtrieu99@gmail.com>
2025-04-09 16:04:05 +0700

    x

---
+++ Minimum_Operations_to_Make_Array_Values_Equal_to_K/Cpp/main.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> nums;
+    int k;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    int minOperations(std::vector<int> &nums, int k) {
+        std::unordered_set<int> mset;
+
+        for (const int &num : nums) {
+            if (num < k) {
+                return -1;
+            }
+            else if (num > k) {
+                mset.insert(num);
+            }
+        }
+
+        return mset.size();
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 5, 2, 5, 4, 5 }, 2 },
+        { { 2, 1, 2 }, 2 },
+        { { 9, 7, 5, 3 }, 1 },
+
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.minOperations(testcase[i].nums, testcase[i].k);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
