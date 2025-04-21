68a22ef8e78408b35ee63be8c08a3c1fa9cd31b1
supnep <nguyenquangtrieu99@gmail.com>
2025-04-21 14:11:57 +0000

    x

---
+++ Count_the_Hidden_Sequences/Cpp/main.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string_view>
+#include <list>
+
+#define LOG_DEBUG(x) (std::cout << x << std::endl)
+
+#define Assert(condition)
+
+struct TestSuit {
+public:
+    std::vector<int> differences;
+    int lower;
+    int upper;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const long long &input) {
+    std::cout << input << std::endl;
+    return output;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const std::vector<T> &input) {
+    std::cout << "[";
+
+    for (auto it = input.begin(); it != input.end(); ++it)
+        if (std::next(it) == input.end())
+            std::cout << std::boolalpha << *it;
+        else
+            std::cout << *it << ",";
+
+    std::cout << "]" << std::endl;
+
+    return output;
+}
+
+class Solution {
+public:
+    int numberOfArrays(std::vector<int> &differences, int lower, int upper) {
+        int x = 0, y = 0, cur = 0;
+        for (int d : differences) {
+            cur += d;
+            x = std::min(x, cur);
+            y = std::max(y, cur);
+            if (y - x > upper - lower) {
+                return 0;
+            }
+        }
+        return (upper - lower) - (y - x) + 1;
+    }
+};
+
+int main() {
+    std::vector<TestSuit> testcase {
+        { { 1, -3, 4 }, 1, 6 },
+        { { 3, -4, 5, 1, -2 }, -4, 5 },
+        { { 4, -7, 2 }, 3, 6 },
+    };
+
+    for (int i = 0; i < testcase.size(); i++) {
+        Solution s;
+        std::cout << ("\033[1;32mTestcase " + std::to_string(i + 1) + "\033[0m") << std::endl;
+        auto r = s.numberOfArrays(testcase[i].differences, testcase[i].lower, testcase[i].upper);
+        std::cout << std::boolalpha << r << std::endl;
+    }
+
+    return 1;
+}
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
